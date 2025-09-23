e97f007b440127c2d044cd3a4d52e0b1ad92343f
FeldLeo <feldleo@outlook.com>
2025-09-23 08:50:40 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+day = 1
+money = 50
+customer_satisfaction = 50
+
+
+@app.route('/')
+def home():
+    print(">>> Rendering NEW index.html")
+    return render_template(
+        'index.html', 
+        day=day,
+        money=money,
+        customer_satisfaction=customer_satisfaction
+    )
+
+@app.route('/action', methods=['POST'])
+def action():
+    global day, money, customer_satisfaction  # allows modifying variables
+
+    # Example: clicking the button decreases money by 10, increases happiness by 5
+    money -= 10
+    employee_happiness += 5
+    day += 1
+
+   
+    money = max(0, min(money, 100)) # keeps values from going below 0 or over 100
+    customer_satisfaction = max(0, min(customer_satisfaction, 100))
+
+    return redirect(url_for('home'))  # reloads home page with updated values
+
+if __name__ == '__main__':
+    app.run(debug=True)
